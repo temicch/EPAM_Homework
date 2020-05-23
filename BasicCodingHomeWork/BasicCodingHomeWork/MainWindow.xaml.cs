9fee6f7885cb9c841be82097bd72378e21861dad
Artyom Maksimov <temicch@gmail.com>
2020-05-24 00:40:54 +0700

    Init

---
+++ BasicCodingHomeWork/BasicCodingHomeWork/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Newton;
+using BinFormatter;
+using System;
+using System.Windows;
+
+namespace BasicCodingHomeWork
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewtonCalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            string valueFormatter = "{0:0.##}";
+            try
+            {
+                double number = double.Parse(NumberTextBox.Text);
+                double degree = double.Parse(DegreeTextBox.Text);
+                NewtonResult.Text = string.Format(valueFormatter, NewtonMethods.Root(number, degree));
+                PowResult.Text = string.Format(valueFormatter, Math.Pow(number, 1 / degree));
+            }
+            catch(Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void BinCalculateButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                uint number = uint.Parse(BinNumberTextBox.Text);
+                BinResult.Text = BinFormatter.BinFormatter.IntToBinString(number);
+                BinNetResult.Text = Convert.ToString(number, 2);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+    }
+}
---
+++ BasicCodingHomeWork/BinFormatter/BinFormatter.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+namespace BinFormatter
+{
+    public static class BinFormatter
+    {
+        public static string IntToBinString(uint value)
+        {
+            if (value == 0)
+                return 0.ToString();
+            StringBuilder result = new StringBuilder(32);
+            while(value > 0)
+            {
+                result.Insert(0, value & 1);
+                value >>= 1;
+            }
+            return result.ToString();
+        }
+    }
+}
