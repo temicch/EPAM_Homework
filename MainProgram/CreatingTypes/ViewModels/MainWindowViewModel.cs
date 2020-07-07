9bb9819a766c4231e839cbec606cec5e5420e1ae
Artyom Maksimov <temicch@gmail.com>
2020-07-08 06:19:19 +0700

    Views became 'Lazy'

--- MainProgram/CreatingTypes/ViewModels/MainWindowViewModel.cs
+++ MainProgram/CreatingTypes/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,7 +12,7 @@ namespace MainProgram.ViewModels
     {
         private readonly MainWindow window = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
 
-        private Dictionary<string, ContentControl> views;
+        private Dictionary<string, Lazy<ContentControl>> views;
 
         public MainWindowViewModel()
         {
@@ -19,31 +20,31 @@ namespace MainProgram.ViewModels
             InitViewModels();
         }
 
-        public List<string> ViewsList => views.Select(x => x.Key).Distinct().ToList();
+        public List<string> ViewsList => views.Select(x => x.Key).ToList();
 
         public BasicCommand OpenViewCommand { get; set; }
 
         private void InitViewModels()
         {
-            views = new Dictionary<string, ContentControl>
+            views = new Dictionary<string, Lazy<ContentControl>>
             {
                 //["01 Unit tests"] = null,
                 //["02 Resource management. Garbage collector"] = null,
-                ["03 Basic C # programming constructs"] = new Task_03View(),
-                ["04 Declaring and calling methods in C #"] = new Task_04View(),
-                ["05 Creating New Types in C #"] = new Task_05View(),
-                ["06 (01) Encapsulation. Overload operations"] = new Task_06_01View(),
-                ["06 (02) Encapsulation. Overload operations"] = new Task_06_02View(),
-                ["06 (03) Inheritance. Interfaces and Abstract Classes"] = new Task_06_03View(),
+                ["03 Basic C # programming constructs"] = new Lazy<ContentControl>(() => new Task_03View()),
+                ["04 Declaring and calling methods in C #"] = new Lazy<ContentControl>(() => new Task_04View()),
+                ["05 Creating New Types in C #"] = new Lazy<ContentControl>(() => new Task_05View()),
+                ["06 (01) Encapsulation. Overload operations"] = new Lazy<ContentControl>(() => new Task_06_01View()),
+                ["06 (02) Encapsulation. Overload operations"] = new Lazy<ContentControl>(() => new Task_06_02View()),
+                ["06 (03) Inheritance. Interfaces and Abstract Classes"] = new Lazy<ContentControl>(() => new Task_06_03View()),
                 //["07 Exception Handling"] = null,
-                ["08 Delegates and Events"] = new Task_08View(),
-                ["09 (01) Collections and Generic Types"] = new Task_09_01View(),
-                ["09 (02) Abstract factory"] = new Task_09_02View(),
-                ["10 LINQ Introduction"] = new Task_10View(),
-                ["11 (01) Garbage collector"] = new Task_11_01View(),
-                ["11 (02) Streams and IO"] = new Task_11_02View(),
-                ["12 XML Technologies"] = new Task_12View(),
-                ["13 ADO.NET EF"] = new Task_13View(),
+                ["08 Delegates and Events"] = new Lazy<ContentControl>(() => new Task_08View()),
+                ["09 (01) Collections and Generic Types"] = new Lazy<ContentControl>(() => new Task_09_01View()),
+                ["09 (02) Abstract factory"] = new Lazy<ContentControl>(() => new Task_09_02View()),
+                ["10 LINQ Introduction"] = new Lazy<ContentControl>(() => new Task_10View()),
+                ["11 (01) Garbage collector"] = new Lazy<ContentControl>(() => new Task_11_01View()),
+                ["11 (02) Streams and IO"] = new Lazy<ContentControl>(() => new Task_11_02View()),
+                ["12 XML Technologies"] = new Lazy<ContentControl>(() => new Task_12View()),
+                ["13 ADO.NET EF"] = new Lazy<ContentControl>(() => new Task_13View()),
                 //["14 Internal device types in .NET Framework. Resource management"] = null,
                 //["15 Web Development Fundamentals"] = null,
                 //["16 Database. Additional materials"] = null,
@@ -62,8 +63,8 @@ namespace MainProgram.ViewModels
 
         private void OpenView(object obj)
         {
-            var viewName = (string) obj;
-            UIElement view = views[viewName];
+            var viewName = (string)obj;
+            UIElement view = views[viewName].Value;
             if (view == null)
             {
                 MessageBox.Show("This task is not implemented yet");
